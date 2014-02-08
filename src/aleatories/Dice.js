058b81e44a4a9e066026d26b3b2e74014ac51b84
Leonardo Val <leonardo.val@creatartis.com>
2014-02-08 11:53:47 -0200

    Distribution caching.

--- src/aleatories/Dice.js
+++ src/aleatories/Dice.js
@@ -1,10 +1,10 @@
 ﻿/** Dice random variables.
 */
-aleatories.Dice = basis.declare(Aleatory, {
+aleatories.Dice = declare(Aleatory, {
 	/** new aleatories.Dice(name, base=6, random=basis.Randomness.DEFAULT):
 		Simple uniform random variable with values in [1, base]. 
 	*/
-	constructor: function Dice(base, next, random) {
+	constructor: function Dice(next, base, random) {
 		Aleatory.call(this, next, random);
 		/** aleatories.Dice.base=6:
 			Amount of different values this dice can take.
@@ -12,6 +12,9 @@ aleatories.Dice = basis.declare(Aleatory, {
 		this.base = isNaN(base) ? 6 : Math.max(2, +base);
 	},
 	
+	/** aleatories.Dice.value():
+		Returns a random value between 1 and base.
+	*/
 	value: function value() {
 		return this.random.randomInt(1, this.base + 1);
 	},
@@ -20,9 +23,10 @@ aleatories.Dice = basis.declare(Aleatory, {
 		Values from 1 to this.base, with uniform probabilities.
 	*/
 	distribution: function distribution() {
-		var prob = 1 / this.base;
-		return basis.Iterable.range(1, this.base + 1).map(function (n, i) {
-			return [n, prob];
-		});
+		return this.__distribution__ || (this.__distribution__ = (function (base) {
+			return Iterable.range(1, base + 1).map(function (n, i) {
+				return [n, 1 / base];
+			}).toArray();
+		})(this.base));
 	}		
 }); // declare Dice.
