69550bd0e6a2dc648191ffd1b8ca91d386ac5b89
Leonardo Val <leonardo.val@creatartis.com>
2014-02-08 11:52:44 -0200

    Support for random games and time limit.

--- src/players/MonteCarloPlayer.js
+++ src/players/MonteCarloPlayer.js
@@ -1,17 +1,53 @@
-﻿/** Automatic players based on Monte Carlo tree search.
+﻿/** Automatic player based on pure Monte Carlo tree search.
 */
-players.MonteCarloPlayer = basis.declare(HeuristicPlayer, {
-	/** new players.MonteCarloPlayer(name, random, simulationCount=30):
-		Builds a player that evaluates its moves using Monte-Carlo 
-		simulations (random games).
+players.MonteCarloPlayer = declare(HeuristicPlayer, {
+	/** new players.MonteCarloPlayer(name, params):
+		Builds a player that chooses its moves using the [pure Monte Carlo game
+		tree search method](http://en.wikipedia.org/wiki/Monte-Carlo_tree_search).
 	*/
-	constructor: function MonteCarloPlayer(name, random, simulationCount) {
-		HeuristicPlayer.call(this, name, random);
-		this.simulationCount = isNaN(simulationCount) ? 30 : +simulationCount >> 0;
+	constructor: function MonteCarloPlayer(name, params) {
+		HeuristicPlayer.call(this, name, params);
+		initialize(this, params)
+		/** players.MonteCarloPlayer.simulationCount=30:
+			Maximum amount of simulations performed for each available move at
+			each decision.
+		*/
+			.integer('simulationCount', { defaultValue: 30, coerce: true })
+		/** players.MonteCarloPlayer.timeCap=1000ms:
+			Time limit for the player to decide.
+		*/
+			.integer('timeCap', { defaultValue: 1000, coerce: true });
+	},
+	
+	/** players.MonteCarloPlayer.selectMoves(moves, game, player):
+		Return an array with the best evaluated moves.
+	*/
+	selectMoves: function selectMoves(moves, game, player) {
+		var monteCarloPlayer = this,
+			endTime = Date.now() + this.timeCap,
+			moves = moves.map(function (move) {
+				return { 
+					move: move, 
+					next: game.next(obj(player, move)), 
+					sum: 0,
+					count: 0
+				};
+			});
+		for (var i = this.simulationCount; i > 0 && Date.now() < endTime; --i) {
+			moves.forEach(function (move) {
+				move.sum += monteCarloPlayer.simulation(move.next, player);
+				++move.count;
+			});
+		}
+		return iterable(moves).greater(function (move) {
+			return move.sum / move.count;
+		}).map(function (move) {
+			return move.move;
+		});
 	},
 	
 	/** players.MonteCarloPlayer.stateEvaluation(game, player):
-		Returns the minimax value for the given game and player.
+		Runs this.simulationCount simulations and returns the average result.
 	*/
 	stateEvaluation: function stateEvaluation(game, player) {
 		var resultSum = 0;
@@ -27,12 +63,21 @@ players.MonteCarloPlayer = basis.declare(HeuristicPlayer, {
 	*/
 	simulation: function simulation(game, player) {
 		var mc = this, move, moves;
-		for (moves = game.moves(); moves; moves = game.moves()) {
-			move = {};
-			game.activePlayers.forEach(function (activePlayer) {
-				move[activePlayer] = mc.random.choice(moves[activePlayer]);
-			});
-			game = game.next(move)
+		while (true) {
+			if (game instanceof Aleatory) {
+				game = game.instantiate();
+			} else {
+				moves = game.moves();
+				if (moves) {
+					move = {};
+					game.activePlayers.forEach(function (activePlayer) {
+						move[activePlayer] = mc.random.choice(moves[activePlayer]);
+					});
+					game = game.next(move);
+				} else {
+					break;
+				}
+			}
 		}
 		return game.result()[player];
 	}
