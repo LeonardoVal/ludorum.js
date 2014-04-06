a405708514b22f15715dd913da913d9d3ce49d14
Leonardo Val <leonardo.val@creatartis.com>
2014-04-06 19:27:00 -0300

    Changed instantiation.
    * instantiate() is gone, and now handled by next() with no arguments.
    * Changed documentation comments.

--- src/Aleatory.js
+++ src/Aleatory.js
@@ -1,9 +1,12 @@
-﻿/** Representation of intermediate game states that depend on some form of 
-	randomness, like: dice, card decks, roulettes, etc.
+﻿/** ## Class `ludorum.Aleatory`
+
+Aleatories are representations of intermediate game states that depend on some 
+form of randomness. `Aleatory` is an abstract class from which different means
+of non determinism can be build, like: dice, card decks, roulettes, etcetera.
 */
 var Aleatory = exports.Aleatory = declare({
-	/** new Aleatory(next, random=basis.Randomness.DEFAULT):
-		Base constructor for a aleatory game state.
+	/** The constructor may take a next function and a random generator (an
+	instance of `basis.Randomness`).
 	*/
 	constructor: function Aleatory(next, random) {
 		this.random = random || Randomness.DEFAULT;
@@ -12,22 +15,9 @@ var Aleatory = exports.Aleatory = declare({
 		}
 	},
 	
-	/** Aleatory.next(value):
-		Returns a new game state given a random value.
-	*/
-	next: function next() {
-		throw new Error((this.constructor.name || 'Aleatory') +".next() not implemented! Please override.");
-	},
-	
-	/** Aleatory.instantiate():
-		Calls this.next() callback with a random value and returns its result.
-	*/
-	instantiate: function instantiate() {
-		return this.next(this.value());
-	},
-	
-	/** Aleatory.value():
-		Calculates a random value for this aleatory.
+	/** The aleatory is always related to a random variable of some sort. The
+	`Aleatory.value()` can be used to obtain a valid random value for that 
+	random variable.
 	*/
 	value: function value() {
 		var n = random.random(), value;
@@ -44,12 +34,18 @@ var Aleatory = exports.Aleatory = declare({
 		return value;
 	},
 	
-	/** Aleatory.distribution():
-		Computes the histogram for the random variables on which this aleatory
-		depends, as an iterable of pairs [value, probability]. Not implemented 
-		by default.
+	/** The function `Aleatory.next(value)` returns the next game state given a 
+	specific value for the random variable. This next game state may also be
+	another `Aleatory`, or the corresponding [`Game`](Game.html) instance.
+	If no value is given, then a random valid value is chosen, using the 
+	`Aleatory.random` randomness generator.
 	*/
-	distribution: function distribution() {
-		throw new Error((this.constructor.name || 'Aleatory') +".distribution() is not implemented! Please override.");
-	}
+	next: unimplemented("Aleatory", "next"),
+	
+	/** In order to properly search a game tree with aleatory nodes, the random
+	variables' distribution have to be known. `Aleatory.distribution()` computes
+	the histogram for the random variables on which this aleatory depends, as a
+	sequence of pairs `[value, probability]`.
+	*/
+	distribution: unimplemented("Aleatory", "distribution")
 }); // declare Aleatory.
