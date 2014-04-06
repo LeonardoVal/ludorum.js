3ed4c38f8df7561d4fe6d346d42b45b0acb9e402
Leonardo Val <leonardo.val@creatartis.com>
2014-04-06 19:28:35 -0300

    Changed documentation comments.

--- src/Player.js
+++ src/Player.js
@@ -1,11 +1,12 @@
-﻿/** Player is the base type for all playing agents.
+﻿/** ## Class `ludorum.Player`
+
+Player is the base type for all playing agents. Basically, playing a game means
+choosing a move from all available ones, each time the game enables the player 
+to do so.
 */
 var Player = exports.Player = declare({
-	/** new Player(params):
-		A player is an agent that plays a game. This means deciding which 
-		move to make from the set of moves available to the player, each 
-		time the game enables the player to do so.
-		This is an abstract class that is meant to be extended.
+	/** The default constructor takes only its `name` from the given `params`.
+	This is an abstract class that is meant to be extended.
 	*/
 	constructor: (function () {
 		var __PlayerCount__ = 0; // Used by the Player's default naming.
@@ -15,9 +16,17 @@ var Player = exports.Player = declare({
 		};
 	})(),
 
-	/** Player.__moves__(game, player):
-		Get the moves in the game for the player, checks if there are any, 
-		and if such is not the case it raises an error.
+	/** A player is asked to choose a move by calling 
+	`Player.decision(game, role)`. The result is the selected move if it can be 
+	obtained synchronously, else a future is returned.
+	*/
+	decision: function decision(game, role) {
+		return this.__moves__(game, role)[0]; // Indeed not a very thoughtful base implementation. 
+	},
+
+	/** To help implement the decision, `Player.__moves__(game, player)` gets
+	the moves in the game for the player. It also checks if there are any moves,
+	and if it not so an error is risen.
 	*/
 	__moves__: function __moves__(game, role) {
 		var moves = game.moves();
@@ -25,38 +34,30 @@ var Player = exports.Player = declare({
 			"Player ", role, " has no moves for game ", game, ".");
 		return moves[role];
 	},
-
-	/** Player.decision(game, player):
-		Ask the player to make a move in the given name for the given player 
-		(role). The result is the selected move if it can be obtained 
-		synchronously, else a Future is returned.
-	*/
-	decision: function decision(game, role) {
-		// Indeed not a very thoughtful base implementation.
-		return this.__moves__(game, role)[0]; 
-	},
-
-	/** Player.participate(match, role):
-		Called when the player joins a match, in order for the player to prepare
-		properly. If this implies building another instance of the player 
-		object, it must be returned in order to participate in the match.
+	
+	/** Before starting a [match](Match.html), all players are asked to join
+	by calling `Player.participate(match, role)`. This allows the player to
+	prepare properly. If this implies building another instance of the player 
+	object, it must be returned in order to participate in the match.
 	*/
 	participate: function participate(match, role) {
 		return this;
 	},
 	
-	// Conversions & presentations. ////////////////////////////////////////////
+	// ### Conversions & presentations #########################################
 
-	/** abstract Player.__serialize__():
-		Returns an array, where the first element should be the name of the 
-		game, and the rest the arguments to call the player's constructor in 
-		order to rebuild this player's state. Default implementation is pretty
-		useless, so please override.
+	/** Players can also be serialized, pretty much in the same way 
+	[games](Game.html) are. `Player.__serialize__()` returns an array, where the 
+	first element should be the name of the game, and the rest the arguments to 
+	call the player's constructor in order to rebuild this player's state.
 	*/
 	__serialize__: function __serialize__() {
 		return [this.constructor.name, {name: this.name}];
 	},
 	
+	/** The string representation of the player is derived straight from its
+	serialization.
+	*/
 	toString: function toString() {
 		var args = this.__serialize__();
 		return args.shift() +'('+ args.map(JSON.stringify).join(',') +')';
--- src/Tournament.js
+++ src/Tournament.js
@@ -1,35 +1,60 @@
-﻿/** A tournament is a set of matches played between many players. The whole 
-	contest ranks the participants according to the result of the matches.
+﻿/** ## Class `ludorum.Tournament`
+
+A tournament is a set of matches played between many players. The whole contest 
+ranks the participants according to the result of the matches. This is an 
+abstract base class for many different types of contests.
 */
 var Tournament = exports.Tournament = declare({
-	/** new Tournament(game, players):
-		Base class of all tournament controllers.
-	*/
 	constructor: function Tournament(game, players) {
-		/** Tournament.game:
-			The game played at the tournament.
+		/** The tournament always has one [`game`](Game.html) state from which 
+		all matches start.
 		*/
 		this.game = game;
-		/** Tournament.players:
-			An array with the participants in the tournament.
+		/** All the [`players`](Player.html) involved in the tournament must be
+		provided to the constructor in an array.
 		*/
 		this.players = Array.isArray(players) ? players : iterables.iterable(players).toArray();
-		/** Tournament.statistics:
-			Tournament statistics. These include the accumulated score for 
-			each player, indexed by name.
-		*/
 		this.statistics = new Statistics();
-		/** Tournament.events:
-			Event handler for this match. Emitted events are: begin, 
-			beforeMatch, afterMatch & end.
-		*/
 		this.events = new Events({ 
 			events: ['begin', 'beforeMatch', 'afterMatch', 'end']
 		});
 	},
 
-	/** Tournament.account(match):
-		Accounts the results of a finished match for the players' score.
+	/** A tournament is made from a sequence of matches, build by 
+	`Tournament.matches()`. It must return an iterable of [`Match`](Match.html) 
+	objects. There isn't a default implementation, so this method must be
+	overridden in all tournament implementations.
+	*/
+	matches: unimplemented("Tournament", "matches"),
+	
+	/** `Tournament.run(matches=this.matches())` plays the given matches, or the
+	ones returned by `Tournament.matches()` by default. Since running a match is
+	asynchronous, running a tournament is too. Hence the result is always a 
+	future, which will be resolved when all matches have been played.
+	*/
+	run: function run(matches) {
+		this.onBegin();
+		var tournament = this;
+		matches = matches || this.matches();
+		return Future.sequence(matches, function (match) {
+			tournament.beforeMatch(match);
+			return match.run().then(function (match) {
+				tournament.account(match);
+				tournament.afterMatch(match);
+				return tournament;
+			});
+		}).then(this.onEnd.bind(this));
+	},
+	
+	/** Tournaments gather information from the played matches using their
+	`statistics` property (instance of `basis.Statistics`). The method 
+	`Tournament.account(match)` is called to accounts the results of each 
+	finished match for the players' score.
+	
+	The match results are gathered in the `results` key. The keys `victories`,
+	`defeats` and `draws` count each result type. The length of each game is
+	recorded under `length`. The move count at each ply is aggregated under
+	`width`. All these numbers are open by game, role, player.
 	*/
 	account: function account(match) {
 		var game = this.game,
@@ -37,8 +62,7 @@ var Tournament = exports.Tournament = declare({
 			isDraw = false,
 			stats = this.statistics;
 		raiseIf(!results, "Match doesn't have results. Has it finished?");
-		// Player statistics.
-		iterable(match.players).forEach(function (p) {
+		iterable(match.players).forEach(function (p) { // Player statistics.
 			var role = p[0],
 				player = p[1],
 				playerResult = results[p[0]],
@@ -59,50 +83,44 @@ var Tournament = exports.Tournament = declare({
 			})
 		});
 	},
-
-	/** Tournament.run(matches=this.matches()):
-		Plays the given matches. This argument must be an Iterable of 
-		ludorum.Match objects.
-	*/
-	run: function run(matches) {
-		this.onBegin();
-		var tournament = this;
-		matches = matches || this.matches();
-		return Future.sequence(matches, function (match) {
-			tournament.beforeMatch(match);
-			return match.run().then(function (match) {
-				tournament.account(match);
-				tournament.afterMatch(match);
-				return tournament;
-			});
-		}).then(this.onEnd.bind(this));
-	},
-
-	/** Tournament.matches():
-		Returns the matches of this contest in an iterable. In this base 
-		implementation this method raises an exception. It must be overriden.
-	*/
-	matches: function matches() {
-		throw new Error("Tournament.matches is not implemented. Please override.");
-	},
 	
-	// Events //////////////////////////////////////////////////////////////////
+	/** ### Events #############################################################
 	
+	Tournaments provide events to enable further analysis and control over it. 
+	`Tournament.events` is the event handler. The emitted events are:
+	*/
+	
+	/** + The `begin` event fired by `Tournament.onBegin()` when the whole 
+	contest begins. The callbacks should have the signature 
+	`function (tournament)`.
+	*/	
 	onBegin: function onBegin() {
 		this.events.emit('begin', this);
 		this.logger && this.logger.info('Tournament begins for game ', game.name, '.');
 	},
 	
+	/** + The `beforeMatch` event triggered by `Tournament.beforeMatch(match)` 
+	just before starting a match. The callbacks should have the signature 
+	`function (match, tournament)`.
+	*/
 	beforeMatch: function beforeMatch(match) {
 		this.events.emit('beforeMatch', match, this);
 		this.logger && this.logger.debug('Beginning match with ', JSON.stringify(match.players), '.');
 	},
 	
+	/** + The `afterMatch` event triggered by `Tournament.afterMatch(match)` 
+	just after a match ends. The callbacks should have the signature 
+	`function (match, tournament)`.
+	*/
 	afterMatch: function afterMatch(match) {
 		this.events.emit('afterMatch', match, this);
 		this.logger && this.logger.debug('Finishing match with ', JSON.stringify(match.players), '.');
 	},
 	
+	/** + The `end` event triggered by `Tournament.onEnd()` when the whole 
+	contest is completed. The callbacks should have the signature 
+	`function (statistics, tournament)`.
+	*/
 	onEnd: function onEnd() {
 		this.events.emit('end', this.statistics, this);
 		this.logger && this.logger.info('Tournament ends for game ', game.name, ':\n', this.statistics, '\n');
