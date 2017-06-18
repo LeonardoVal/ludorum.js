1cbde5251f3a7a12f7284003e0d0739aae92a58a
Leonardo Val <leonardo.val@creatartis.com>
2017-06-18 19:37:31 -0300

    Benchmarking
    + Most players with most games. Takes a while, but its interesting.

--- tests/perf/players.perf.js
+++ tests/perf/players.perf.js
@@ -1,27 +1,72 @@
 var base = require('creatartis-base'),
 	Sermat = require('sermat'),
-	ludorum = require('../../build/ludorum');
+	ludorum = require('../../build/ludorum'),
+	games = ludorum.games,
+	players = ludorum.players;
 Sermat.modifiers.mode = Sermat.CIRCULAR_MODE;
-	
+
+module.exports = {
+	name: 'Player benchmarks',
+	tests: { /* To be loaded later. */ }
+};
+
 // Test cases //////////////////////////////////////////////////////////////////////////////////////
 
+var RANDOM = base.Randomness.DEFAULT;
+
 function randomGameStates(game) {
-	var r = [];
-	for (var ms; (ms = game.possibleMoves()).length; ) {
-		r.push(game);
-		game = game.next(base.Randomness.DEFAULT.choice(ms));
+	var r = {}, count = 0,
+		g, ms;
+	
+	for (var i = 0; i < 10 && Object.keys(r).length < 50; i++) {
+		g = game;
+		while (g.isContingent || !g.result()) {
+			if (g.isContingent) {
+				g = g.randomNext();
+			} else {
+				r[Sermat.hashCode(g).toString(36)] = g;
+				ms = g.possibleMoves();
+				g = g.next(RANDOM.choice(ms));
+			}
+		}
 	}
-	return r;
+	return Object.values(r);
 }
 
-var TICTACTOE_STATES = randomGameStates(new ludorum.games.TicTacToe());
+var GAMES = [
+		new games.ToadsAndFrogs(),
+		new games.TicTacToe(),
+		new games.Pig(),
+		new games.OddsAndEvens(15),
+		new games.Bahab(),
+		new games.Puzzle15()
+	],
+	PLAYERS = [new players.RandomPlayer({ name: 'RandomPlayer' })]
+		.concat([1,3,5].map(function (h) {
+			return new players.AlphaBetaPlayer({ name: 'MM\u03b1\u03b2(h='+ h +')', horizon: h });
+		}))
+		.concat([1,3,5].map(function (h) {
+			return new players.MaxNPlayer({ name: 'MaxN(h='+ h +')', horizon: h });
+		}))
+		.concat([10,50,100].map(function (s) {
+			return new players.MonteCarloPlayer({ name: 'MCTS(s='+ s +')', simulationCount: s });
+		}))
+		.concat([10,50,100].map(function (s) {
+			return new players.UCTPlayer({ name: 'UCT(s='+ s +')', simulationCount: s });
+		}));
+
+GAMES.forEach(function (game) {	
+	var randomStates = randomGameStates(game);
+	game.constructor.RANDOM_STATES = randomStates;
+	console.log("Generated "+ randomStates.length +" random game states for "+ game.name +".");
+});
 
 function playerDecisionTest(player, gameStates) {
 	return {
 		defer: true,
 		fn: function (deferred) {
 			return base.Future.all(gameStates.map(function (state) {
-				return player.decision(state, state.activePlayer());
+				return player.decision(state, RANDOM.choice(state.activePlayers));
 			})).then(function () {
 				deferred.resolve();
 			});
@@ -29,14 +74,9 @@ function playerDecisionTest(player, gameStates) {
 	};
 }
 
-// Exports /////////////////////////////////////////////////////////////////////////////////////////
-
-module.exports = {
-	name: 'Player benchmarks',
-	tests: {
-		'RandomPlayer with TicTacToe':
-			playerDecisionTest(new ludorum.players.RandomPlayer(), TICTACTOE_STATES),
-		'MCTS(s=10) with TicTacToe':
-			playerDecisionTest(new ludorum.players.MonteCarloPlayer({ simulationCount: 10 }), TICTACTOE_STATES)
+base.Iterable.product(GAMES, PLAYERS).forEachApply(function (game, player) {
+	if (player.isCompatibleWith(game)) {
+		module.exports.tests[game.name +' with '+ player.name] = playerDecisionTest(player, 
+			game.constructor.RANDOM_STATES);
 	}
-};
+})
