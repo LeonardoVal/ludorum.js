43bf401a5abaae47d4d9ba624f3ba95564547a38
Leonardo Val <leonardo.val@creatartis.com>
2017-09-14 10:55:01 -0300

    RequireJS config
    + Moved to a separate script, so it can be shared.

---
+++ tests/require-config.js
@@ -0,0 +1,8 @@
+require.config({
+	paths: {
+		"ludorum": "../build/ludorum",
+		"creatartis-base": "../node_modules/creatartis-base/build/creatartis-base.min",
+		"sermat": "../node_modules/sermat/build/sermat-umd-min",
+		"playtester": "../build/playtester-common"
+	},
+});
