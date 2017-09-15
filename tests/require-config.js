feb1ff22e783ad14b3bdd262c81a9a03367734d7
Leonardo Val <leonardo.val@creatartis.com>
2017-09-15 13:50:28 -0300

    Log for RequireJS configuration

--- tests/require-config.js
+++ tests/require-config.js
@@ -1,8 +1,12 @@
-require.config({
-	paths: {
-		"ludorum": "../build/ludorum",
-		"creatartis-base": "../node_modules/creatartis-base/build/creatartis-base.min",
-		"sermat": "../node_modules/sermat/build/sermat-umd-min",
-		"playtester": "../build/playtester-common"
-	},
-});
+(function () { "use strict";
+	var config = {
+		paths: {
+			"ludorum": "../build/ludorum",
+			"creatartis-base": "../node_modules/creatartis-base/build/creatartis-base.min",
+			"sermat": "../node_modules/sermat/build/sermat-umd-min",
+			"playtester": "../build/playtester-common"
+		}
+	};
+	require.config(config);
+	console.log("RequireJS configuration: "+ JSON.stringify(config, null, '  '));
+})();
