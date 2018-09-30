3ae97168dd2011492af40548859c34476a053c44
Leonardo Val <leonardo.val@creatartis.com>
2018-09-30 10:30:53 -0300

    Fixed random plays + Plays were only played by random players, due to an error in setting up the `players` array for the match. + Separated draw and defeat counts. + _Main_ function now runs many tests with different horizons and levels.

--- tests/random-assessment.js
+++ tests/random-assessment.js
@@ -1,4 +1,5 @@
 var base = require('creatartis-base'),
+	Sermat = require('sermat'),
 	ludorum = require('../build/ludorum');
 
 // Synonyms
@@ -8,43 +9,58 @@ var Iterable = base.Iterable,
 	Match = ludorum.Match,
 	RandomPlayer = ludorum.players.RandomPlayer
 ;
+Sermat.modifiers.mode = Sermat.CIRCULAR_MODE;
 
-function randomPlays(game, n, player) {
-	var randomPlayers = game.players.map(function () {
-			return new RandomPlayer();
+function randomPlays(game, level, player) {
+	var randomPlayers = game.players.map(function (p, i) {
+			return new RandomPlayer({ name: 'RAN#'+ (i + 1) });
 		}),
-		matches = Iterable.range(n).product(game.players).mapApply(function (i, role) {
+		matches = Iterable.range(level).product(game.players).mapApply(function (i, role) {
 			var players = randomPlayers.slice();
-			players[role] = player;
+			players[game.players.indexOf(role)] = player;
 			var match = new Match(game, players);
 			return match.run().then(function () {
 				var result = match.result();
+				//console.log("---", role, result, players.map((p)=> p.constructor.name));//FIXME
 				return [role, result[role]];
 			});
-		}),
-		results = iterable(game.players).map(function (role) {
-			return [role, Infinity];
-		}).toObject();
-	return Future.sequence(matches, function (r) {
-		results[r[0]] = Math.min(results[r[0]], r[1]);
+		});
+	return Future.all(matches).then(function (rs) {
+		var results = iterable(game.players).map(function (role) {
+				return [role, Infinity];
+			}).toObject();
+		iterable(rs).forEachApply(function (role, r) {
+			results[role] = Math.min(results[role], r);
+		});
 		return results;
 	});
 }
 
-function assess(game, n, player) {
+function assess(n, game, level, player) {
 	var results = iterable(game.players).map(function (role) {
-			return [role, [0, 0]];
+			return [role, [0, 0, 0]];
 		}).toObject();
-	return Future.sequence(Iterable.range(10), function () {
-		return randomPlays(game, n, player).then(function (rs) {
+	return Future.sequence(Iterable.range(n), function () {
+		return randomPlays(game, level, player).then(function (rs) {
 			for (var role in results) {
-				results[role][rs[role] > 0 ? 0 : 1]++;
+				results[role][Math.sign(rs[role]) + 1]++;
 			}
 			return results;
 		});
 	});
 }
 
-assess(new ludorum.games.TicTacToe(), 2,
-	new ludorum.players.AlphaBetaPlayer({ horizon: 4 })
-).then(console.log);
+(function main() { /////////////////////////////////////////////////////////////////////////////////
+	var game = new ludorum.games.TicTacToe();
+	return Future.sequence([0, 2, 4, 6, 8, 10], function (horizon) {
+		var player = new ludorum.players.MiniMaxPlayer({
+				name: 'MM'+ horizon,
+				horizon: horizon
+			});
+		return Future.sequence(Iterable.range(1, 11), function (level) {
+			return assess(100, game, level, player).then(function (assessment) {
+				console.log("level:"+ level +"\t"+ Sermat.ser(player) +"\t"+ Sermat.ser(assessment));
+			});
+		});
+	});
+})();
