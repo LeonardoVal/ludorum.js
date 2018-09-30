76cc366db3f854e6c4e225ca0f50aa02afd2dfe6
Leonardo Val <leonardo.val@creatartis.com>
2018-09-30 18:56:48 -0300

    Distributed playthroughs + Brought to you by _Capataz_.

--- tests/random-assessment.js
+++ tests/random-assessment.js
@@ -1,5 +1,6 @@
 var base = require('creatartis-base'),
 	Sermat = require('sermat'),
+	capataz = require('capataz'),
 	ludorum = require('../build/ludorum');
 
 // Synonyms
@@ -11,17 +12,22 @@ var Iterable = base.Iterable,
 ;
 Sermat.modifiers.mode = Sermat.CIRCULAR_MODE;
 
-function randomPlays(game, level, player) {
+function localRunMatch(game, players) {
+	var match = new Match(game, players);
+	return match.run().then(function () {
+		return match.result();
+	});
+}
+
+function randomPlays(game, level, player, runMatch) {
+	runMatch = runMatch || localRunMatch;
 	var randomPlayers = game.players.map(function (p, i) {
 			return new RandomPlayer({ name: 'RAN#'+ (i + 1) });
 		}),
 		matches = Iterable.range(level).product(game.players).mapApply(function (i, role) {
 			var players = randomPlayers.slice();
 			players[game.players.indexOf(role)] = player;
-			var match = new Match(game, players);
-			return match.run().then(function () {
-				var result = match.result();
-				//console.log("---", role, result, players.map((p)=> p.constructor.name));//FIXME
+			return runMatch(game, players).then(function (result) {
 				return [role, result[role]];
 			});
 		});
@@ -36,12 +42,12 @@ function randomPlays(game, level, player) {
 	});
 }
 
-function assess(n, game, level, player) {
+function assess(n, game, level, player, runMatch) {
 	var results = iterable(game.players).map(function (role) {
 			return [role, [0, 0, 0]];
 		}).toObject();
 	return Future.sequence(Iterable.range(n), function () {
-		return randomPlays(game, level, player).then(function (rs) {
+		return randomPlays(game, level, player, runMatch).then(function (rs) {
 			for (var role in results) {
 				results[role][Math.sign(rs[role]) + 1]++;
 			}
@@ -51,15 +57,39 @@ function assess(n, game, level, player) {
 }
 
 (function main() { /////////////////////////////////////////////////////////////////////////////////
+	var server = capataz.Capataz.run({
+			port: 8088,
+			workerCount: 2,
+			desiredEvaluationTime: 10000,
+			logFile: base.Text.formatDate(null, '"./tests/random-assessment-"yyyymmdd-hhnnss".log"')
+		}),
+		logger = server.logger;
+	server.__serveNodeModule__(Sermat);
+	server.__serveNodeModule__(ludorum);
+
+	function distributedRunMatch(game, players) {
+		return server.schedule({
+			info: game.name +'('+ players.map(function (p) {
+					return p.name;
+				}).join(',') +')',
+			imports: ['sermat', 'ludorum'],
+			args: [Sermat.ser(game), Sermat.ser(players)],
+			fun: 'function (Sermat, ludorum, game, players) {\n\t'+
+				'var m = new ludorum.Match(Sermat.mat(game), Sermat.mat(players));\n\t'+
+				'return m.run().then(function () { return m.result(); });\n'+
+			'}'
+		});
+	}
+
 	var game = new ludorum.games.TicTacToe();
-	return Future.sequence([0, 2, 4, 6, 8, 10], function (horizon) {
+	return Future.sequence([1, 2, 4, 6, 8, 10], function (horizon) {
 		var player = new ludorum.players.MiniMaxPlayer({
 				name: 'MM'+ horizon,
 				horizon: horizon
 			});
 		return Future.sequence(Iterable.range(1, 11), function (level) {
-			return assess(100, game, level, player).then(function (assessment) {
-				console.log("level:"+ level +"\t"+ Sermat.ser(player) +"\t"+ Sermat.ser(assessment));
+			return assess(1000, game, level, player, distributedRunMatch).then(function (assessment) {
+				logger.info("level:"+ level +"\t"+ player.name +"\t"+ JSON.stringify(assessment));
 			});
 		});
 	});
