762d2a84bc014cee81a0f59061b409f181e8bc27
Leonardo Val <leonardo.val@creatartis.com>
2018-10-26 11:24:16 -0300

    Updated dependencies + Dependency on `creatartis-base` move to `creatartis` scope.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 	});
 
-	require('creatartis-grunt').config(grunt, {
+	require('@creatartis/creatartis-grunt').config(grunt, {
 		sourceNames: ['__prologue__',
 			'Game', 'Player', 'Match', 'Contingent', 'Tournament',
 			// utils.
