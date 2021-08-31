eb27ff458f44a401c8ee0acdc150213f83b6a110
Leonardo Val <leonardo.val@creatartis.com>
2021-08-30 21:13:03 -0300

    Add player packages

---
+++ packages/player-montecarlo/src/index.js
@@ -0,0 +1,11 @@
+import aleatories from './aleatories';
+import games from './games';
+import Match from './Match';
+import players from './players';
+
+export {
+  aleatories,
+  games,
+  Match,
+  players,
+};
