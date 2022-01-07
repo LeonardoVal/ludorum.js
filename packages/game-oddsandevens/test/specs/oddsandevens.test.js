eabd4ecb06dab9ff24bd1253845556c778734735
Leonardo Val <leonardo.val@creatartis.com>
2022-01-07 00:22:34 -0300

    Add player tests for Odds & Evens

--- packages/game-oddsandevens/test/specs/oddsandevens.test.js
+++ packages/game-oddsandevens/test/specs/oddsandevens.test.js
@@ -1,6 +1,12 @@
 import MersenneTwister from '@creatartis/randomness/generators/MersenneTwister';
 import { tests as gameTests } from '@ludorum/core/games';
 import { tests as playerTests } from '@ludorum/core/players';
+import {
+  AlphaBetaPlayer, MaxNPlayer, MiniMaxPlayer,
+} from '@ludorum/players-minimax/players';
+import {
+  MonteCarloPlayer,
+} from '@ludorum/players-montecarlo/players';
 import { OddsAndEvens } from '../../src/games';
 
 const RANDOM = new MersenneTwister(parseInt('Pig', 32) % 1e8);
@@ -21,6 +27,33 @@ describe('OddsAndEvens', () => {
         zeroSum: true,
         update: i % 2 === 0,
       });
+    }
+  });
+
+  it('cannot be played with minimax players', () => {
+    const game = new OddsAndEvens();
+    const playerBuilders = [
+      () => new MaxNPlayer({ random: RANDOM }),
+      () => new MiniMaxPlayer({ random: RANDOM }),
+      () => new AlphaBetaPlayer({ random: RANDOM }),
+    ];
+    for (const playerBuilder of playerBuilders) {
+      const player = playerBuilder();
+      expect(player.canPlay(game)).toBe(false);
+    }
+  });
+
+  it('can be played with montecarlo players', async () => {
+    const game = new OddsAndEvens();
+    const playerBuilders = [
+      () => new MonteCarloPlayer({ random: RANDOM }),
+    ];
+    for (const playerBuilder of playerBuilders) {
+      const player = playerBuilder();
+      expect(player.canPlay(game)).toBe(true);
+      for (let i = 0; i < MATCH_COUNT; i += 1) {
+        await playerTests.checkPlayer({ game, playerBuilder });
+      }
     }
   });
 }); // describe 'OddsAndEvens'
